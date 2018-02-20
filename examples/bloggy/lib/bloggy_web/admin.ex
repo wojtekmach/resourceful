3a0379c1f42ce8ce0f738d6464c38d982320859f
Wojtek Mach <wojtek@wojtekmach.pl>
2018-02-21 00:22:26 +0100

    Nest modules

--- examples/bloggy/lib/bloggy_web/admin.ex
+++ examples/bloggy/lib/bloggy_web/admin.ex
@@ -1,5 +1,5 @@
 defmodule BloggyWeb.Admin do
   import Resourceful
 
-  resource Elixir.Bloggy.Blog, Elixir.Bloggy.Blog.Post, "posts", repo: Bloggy.Repo
+  resource Blog, Blog.Post, "posts", repo: Bloggy.Repo
 end
--- examples/bloggy/test/bloggy_web/admin_test.exs
+++ examples/bloggy/test/bloggy_web/admin_test.exs
@@ -1,27 +1,28 @@
 defmodule BloggyWeb.AdminTest do
   use Bloggy.DataCase, async: true
+  alias BloggyWeb.Admin.{Blog, Blog.Post}
 
   test "schema" do
-    assert Bloggy.Blog.Post.__schema__(:types) == %{id: :id, title: :string, body: :string}
+    assert Blog.Post.__schema__(:types) == %{id: :id, title: :string, body: :string}
   end
 
   test "context" do
-    assert [] = Bloggy.Blog.list()
+    assert [] = Blog.list()
 
-    {:ok, _} = Bloggy.Blog.create(%{title: "Hello", body: "Hello World!"})
+    {:ok, _} = Blog.create(%{title: "Hello", body: "Hello World!"})
 
-    [post] = Bloggy.Blog.list()
+    [post] = Blog.list()
     assert post.title == "Hello"
     assert post.body == "Hello World!"
 
-    post = Bloggy.Blog.get!(post.id)
+    post = Blog.get!(post.id)
     assert post.title == "Hello"
 
-    {:ok, updated} = Bloggy.Blog.update(post, %{body: "Good evening!"})
+    {:ok, updated} = Blog.update(post, %{body: "Good evening!"})
     assert updated.body == "Good evening!"
 
-    {:ok, %Bloggy.Blog.Post{}} = Bloggy.Blog.delete(updated)
+    {:ok, %Post{}} = Blog.delete(updated)
 
-    assert [] = Bloggy.Blog.list()
+    assert [] = Blog.list()
   end
 end
