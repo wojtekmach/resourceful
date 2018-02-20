88a7ecbc8aeacb2e19c900fb9bb3add1f23349ec
Wojtek Mach <wojtek@wojtekmach.pl>
2018-02-20 23:05:12 +0100

    Cleanup

--- lib/resourceful.ex
+++ lib/resourceful.ex
@@ -1,18 +1,2 @@
 defmodule Resourceful do
-  @moduledoc """
-  Documentation for Resourceful.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Resourceful.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/resourceful_test.exs
+++ test/resourceful_test.exs
@@ -1,8 +1,6 @@
 defmodule ResourcefulTest do
   use ExUnit.Case
-  doctest Resourceful
 
   test "greets the world" do
-    assert Resourceful.hello() == :world
   end
 end
